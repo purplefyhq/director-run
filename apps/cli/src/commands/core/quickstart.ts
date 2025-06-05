dda47a14c40234b86fd7b2294a3f586e44b1c258
Barnaby Malet <480673+barnaby@users.noreply.github.com>
2025-06-05 12:01:47 +0200

    v0.0.16 (#139)

--- apps/cli/src/commands/core/quickstart.ts
+++ apps/cli/src/commands/core/quickstart.ts
@@ -1,7 +1,11 @@
 import { DirectorCommand } from "@director.run/utilities/cli/director-command";
 import { actionWithErrorHandler } from "@director.run/utilities/cli/index";
+import { getLogger } from "@director.run/utilities/logger";
+import { env } from "../../env";
 import { startGateway } from "./serve";
 import { openStudio } from "./studio";
+
+const logger = getLogger("quickstart");
 
 export function registerQuickstartCommand(program: DirectorCommand) {
   program
@@ -10,6 +14,9 @@ export function registerQuickstartCommand(program: DirectorCommand) {
     .action(
       actionWithErrorHandler(async () => {
         await startGateway(async () => {
+          logger.info(
+            `gateway started, opening ${env.STUDIO_URL} in your browser...`,
+          );
           await openStudio();
         });
       }),
