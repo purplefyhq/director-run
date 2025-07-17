6ad9374a9147153587efbafa204bd62001ea9469
Tom Bates <hey@tombates.co>
2025-07-17 13:48:37 -0300

    Neon for prod (#259)
    * is this faster?
    
    * woops
    
    * Clean

--- apps/registry/src/db/client.ts
+++ apps/registry/src/db/client.ts
@@ -1,12 +1,21 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { isProduction } from "@director.run/utilities/env";
+import { drizzle as NeonDrizzle } from "drizzle-orm/neon-http";
+import { drizzle as LocalDrizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
 
-export const client = new Pool({
-  connectionString: process.env.DATABASE_URL as string,
-});
+function getDb() {
+  if (
+    isProduction() &&
+    typeof process.env.DATABASE_URL === "string" &&
+    process.env.DATABASE_URL.includes("neon")
+  ) {
+    return NeonDrizzle(process.env.DATABASE_URL, { schema });
+  } else {
+    return LocalDrizzle(process.env.DATABASE_URL as string, { schema });
+  }
+}
 
-export const db = drizzle(client, { schema });
+export const db = getDb();
 
 export type DatabaseConnection = typeof db;
